5c30bc7e659c97b521c7e2146e9b6eb96bdd6fec
GG <zjjhgzh@gmail.com>
2022-12-29 16:35:07 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import AutoImport from "unplugin-auto-import/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // base: "/distribution_management_system/",
   plugins: [
     vue(),
     AutoImport({
